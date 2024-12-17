14cbdf3c455cfd12d10052a6a08c9a323e120fc8
Chase LaBarre <86638507+ObfuscatedFuture@users.noreply.github.com>
2024-12-17 16:55:26 -0500

    Added new stack_info file storing player stacks after each hands allowing for stack over time graphing.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def main(arg):
-    print(f"Ver: : {arg}")
+    print(f"{arg}")
     print(f"Created By : CodeSlug")
     full_log = "testData.csv"
     ledger = "ledger.csv"
@@ -28,17 +28,17 @@ def main(arg):
     # Extract matched parts and assign them to new columns
     data['amount'] = data['entry'].str.extract(regex)
     # Fill unmatched rows with empty strings
-    data['amount'] = data['amount'].fillna(-1)
+    data['amount'] = data['amount'].fillna('')
 
     # Formats the action column
-    action_pattern = r"\b(calls|folds|raises|bets|checks|shows|collected|returned|posts|starting hand|ending hand|stand up|joined the game with a|quits the game)\b"
+    action_pattern = r"\b(calls|folds|raises|bets|checks|shows|collected|returned|posts|starting hand|ending hand|stand up|quits the game|change|participation)\b"
     data['action'] = data['entry'].str.extract(action_pattern)
     data['action'] = data['action'].str.replace('returned', 'return')
     data['action'] = data['action'].str.replace('collected', 'collects')
     data['action'] = data['action'].str.replace('starting hand', 'new hand')
     data['action'] = data['action'].str.replace('ending hand', 'end hand')
     data['action'] = data['action'].str.replace('stand up', 'leaves')
-    data['action'] = data['action'].str.replace('joined the game with a', 'joins')
+    data['action'] = data['action'].str.replace('participation', 'joins')
     data['action'] = data['action'].str.replace('quits the game', 'leaves')
     data['action'] = data['action'].fillna("").astype("string")
 
@@ -55,7 +55,7 @@ def main(arg):
     data['hand_count'] = data['hand_count'].fillna(0).astype(int)
 
     data['at'] = pd.to_datetime(data['at'])
-    data['amount'] = data['amount'].astype(float)
+    data['amount'] = pd.to_numeric(data['amount'], errors='coerce')
 
     # Potential actions list:
     # checks, calls, raises, bets, folds
@@ -100,7 +100,7 @@ def main(arg):
         player_data = player_data[player_data['action'].isin(['joins', 'leaves'])]
 
         recent = None
-        player_data.sort_values(by=['hand_count'], inplace=True, ascending=True)
+        player_data = player_data.sort_values(by=['at'], ascending=True)
 
         # TODO make this code more efficient and add time tracking
         for index, row in player_data.iterrows():
@@ -117,7 +117,9 @@ def main(arg):
 
         # TODO add time played column to modified_ledger.csv
         # TODO add vpip? column to modified_ledger.csv
+######################
 
+#######################################
     # Processing for stack_info dataFrame
     def process_stacks(row):
         if row.startswith("Player stacks:"):
@@ -146,19 +148,38 @@ def main(arg):
         data[['hand_count']].loc[stack_rows.index].reset_index(drop=True),
         stack_info
     ], axis=1)
-    stack_info = stack_info.query("Player < 'ffffffffff'")
-    # stack_info.to_csv('stack_info.csv', index = False)
+
+    data = data.sort_values(by=['at'], ascending=True)
+    stack_info.sort_values(by=['hand_count'], ascending=True)
+
+    df_join = data[data['action'] == 'joins']
+
+    df_join = df_join[['player_name', 'amount', 'hand_count']]
+
+    players_adjustments = {}
+    for index, row in df_join.iterrows():
+        player = row['player_name']
+        amount = row['amount']
+        hand = row['hand_count']
+
+        s = (stack_info['hand_count'] > hand) & (stack_info['Player'] == player)
+
+        stack_info.loc[s, 'Profit'] = stack_info.loc[s, 'Stack'] - amount
+
+    stack_info['Profit'] = stack_info['Profit'].round(2)
 
     plt.figure(figsize=(10, 6))
-
+    top_ten_names = stack_info['Player'].unique()[:1]
+    filteredSI = stack_info[stack_info['Player'].isin(top_ten_names)]
     # Group by player and plot
-    for player, group in stack_info.groupby('Player'):
+    for player, group in filteredSI.groupby('Player'):
+        plt.plot(group['hand_count'], group['Profit'], marker='o', label=player)
         plt.plot(group['hand_count'], group['Stack'], marker='o', label=player)
 
     # Add labels, title, and legend
-    plt.ylabel('Stack', fontsize=14)
+    plt.ylabel('Profit', fontsize=14)
     plt.xlabel('Hand Count', fontsize=14)
-    plt.title('Player Stacks vs. Hand Count', fontsize=16)
+    plt.title('Player Profit vs. Hand Count', fontsize=16)
     plt.legend(title="Player", bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=10)
     plt.grid(True)
     plt.tight_layout()
@@ -166,11 +187,14 @@ def main(arg):
     # Show the plot
     plt.show()
 
+
+
     series = pd.Series(played)
     combined_players["Hands_Played"] = series
 
     combined_players.to_csv('modified_ledger.csv', index=False)
     data.to_csv('modified_data.csv', index=False)
+    stack_info.to_csv('stack_info.csv', index=False)
 
 if __name__ == '__main__':
     main('Ver 0.1')
